349a3bfa2a88833b2737365b10f12110330aef52
Jimi <jimi.vihinen@hotmail.com>
2023-11-27 21:14:22 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<header>
+    <div id="topLayout">
+        <h1 id="title">NotesApp</h1>
+    </div>
+</header>
+<body>
+    <main>
+        <slot></slot>
+    </main> 
+</body>
+<footer>
+    <p id="copyright">© 2023 NotesApp</p>
+</footer>
+
+
+<style>
+    #title{
+        text-align: center;
+        margin-left: 20px;
+        margin-right: 20px;
+        width: 150px;
+
+        border: solid 2px black;
+    }
+    #copyright{
+        text-align: center;
+        margin-top: 50px;
+    }
+    #topLayout{
+        border-bottom: solid 2px black;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+    let isDown = false;
+    const mouseDown = (element) => {
+      console.log(element.target.id);
+
+      let elementID = element.target.id;
+      isDown = true;
+
+
+      document.getElementById(elementID).classList.remove("button");
+
+      console.log("done");
+      
+    }
+
+    const mouseUp = (element) => {
+        
+      
+    }
+
+  </script>
+  
+  <div class="button" class:down={isDown} id="addNotes" on:mousedown={mouseDown} on:mouseup={}>Create notes for class</div>
+
+
+  <style>
+    .button {
+      border: solid 2px black;
+      border-radius: 5px;
+      text-align: center;
+      width: 150px;
+      box-shadow: 0px 5px 0px 0px;
+    }
+  
+    .button:hover {
+      cursor: pointer;
+      transform: translateY(2px);
+      box-shadow: 0px 3px 0px 0px;
+    }
+
+    .down {
+        border: solid 2px black;
+      border-radius: 5px;
+      text-align: center;
+      width: 150px;
+      transform: translateY(5px);
+      box-shadow: 0px 0px 0px 0px;
+    }
+    .mouseDown:hover{
+        cursor: pointer;
+    }
+
+  </style>
+  
+
+
+
+
